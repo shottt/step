63046a96b10be8cda6020346a3db12f6026eb343
shottt <futuregadgetlabomem@gmail.com>
2021-07-04 22:25:00 +0900

    docker-compose管理へ変更と軽微な修正

--- resources/js/components/organisms/feature.vue
+++ resources/js/components/organisms/feature.vue
@@ -3,13 +3,18 @@
     <h2 class="p-feature__title">3つの特徴</h2>
      <div class="p-feature__content">
         <div class="p-feature__item">
-          STEPの投稿が可能
+          <i class="fas fa-edit fa-5x"></i>
+          <h3>STEP投稿</h3>
+          <p>スキルを習得するために、自分が学んだおすすめの順番（STEP）を投稿することができます。</p>
         </div>
         <div class="p-feature__item">
-          STEPの共有が可能
+          <i class="fas fa-share-square fa-5x"></i>
+          <h3>STEP共有</h3>
+          <p>Twitterで「STEP」を不特定多数の人にシェアすることができます。</p>
         </div>
         <div class="p-feature__item">
-          STEPのチャレンジができる
+          <h3>STEPチャレンジ</h3>
+          <p>色々な人が投稿したSTEPを見ることができ、チャレンジすることもできます。</p>
         </div>
      </div>
     
